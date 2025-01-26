f7b48ab92651776884973bdbf8b0ce8f08e63213
Oleg <2695198+heilgar@users.noreply.github.com>
2025-01-26 18:10:11 -0500

    stage: highlight

--- lua/bookmarks/autocmds.lua
+++ lua/bookmarks/autocmds.lua
@@ -44,6 +44,8 @@ function Autocmds.refresh_buffer(bufnr)
         return
     end
 
+    -- Utils.debug_print('Refresh buffer called')
+
     -- Skip special buffers
     if vim.bo[bufnr].buftype ~= "" then
         -- Utils.debug_print(string.format("Skipping special buffer %d", bufnr))
@@ -52,10 +54,6 @@ function Autocmds.refresh_buffer(bufnr)
 
     local list = load_buffer_bookmarks(bufnr)
     if list then
-        -- Utils.debug_print(string.format("Signs about to be placed for buffer %d", bufnr))
-        Decorations.place_signs(bufnr, list)
-        Decorations.highlight_lines(bufnr, list)
-        -- Utils.debug_print(string.format("Signs placed for buffer %d", bufnr))
         vim.defer_fn(function()
             if vim.api.nvim_buf_is_valid(bufnr) then
                 -- Utils.debug_print(string.format("Deferred sign refresh for buffer %d", bufnr))
@@ -68,24 +66,6 @@ end
 
 function Autocmds.setup()
     local group = vim.api.nvim_create_augroup("BookmarksAutocmds", { clear = true })
-    -- vim.api.nvim_create_autocmd("BufWritePost", {
-    --     group = group,
-    --     callback = function(args)
-    --         local bufnr = args.buf
-    --         -- Utils.debug_print(string.format("BufWritePost triggered for buffer %d", bufnr))
-    --         if vim.bo[bufnr].buftype ~= "" or vim.bo[bufnr].filetype == "NvimTree" then
-    --             return
-    --         end
-    --         -- Refresh immediately
-    --         Autocmds.refresh_buffer(bufnr)
-    --         -- And schedule another refresh
-    --         vim.defer_fn(function()
-    --             if vim.api.nvim_buf_is_valid(bufnr) then
-    --                 Autocmds.refresh_buffer(bufnr)
-    --             end
-    --         end, 50)
-    --     end,
-    -- })
     -- Refresh on file write
     vim.api.nvim_create_autocmd("BufWritePost", {
         group = group,
@@ -94,23 +74,12 @@ function Autocmds.setup()
             if vim.bo[bufnr].buftype ~= "" then
                 return
             end
-            Autocmds.refresh_buffer(bufnr)
+
+            vim.defer_fn(function()
+                Autocmds.refresh_buffer(bufnr)
+            end, 200)
         end,
     })
-    -- vim.api.nvim_create_autocmd({ "BufEnter", "BufRead" }, {
-    --     group = group,
-    --     callback = function(args)
-    --         local bufnr = args.buf
-    --         -- Skip special buffers (including NvimTree)
-    --         if vim.bo[bufnr].buftype ~= "" or vim.bo[bufnr].filetype == "NvimTree" then
-    --             -- Utils.debug_print(string.format("Skipping special buffer/filetype %d", bufnr))
-    --             return
-    --         end
-    --
-    --         Autocmds.refresh_buffer(args.buf)
-    --     end,
-    -- })
-    --
     -- Refresh on buffer enter/read
     vim.api.nvim_create_autocmd({ "BufEnter", "BufRead" }, {
         group = group,
@@ -134,22 +103,6 @@ function Autocmds.setup()
             end
         end,
     })
-
-    -- vim.api.nvim_create_autocmd({ "BufDelete" }, {
-    --     group = group,
-    --     callback = function(args)
-    --         local bufnr = args.buf
-    --         -- Utils.debug_print(string.format("Buffer %d left/deleted, cleaning up", bufnr))
-    --         buffer_bookmarks[args.buf] = nil
-    --
-    --         -- Clear signs and highlights when leaving buffer
-    --         if vim.api.nvim_buf_is_valid(bufnr) then
-    --             vim.fn.sign_unplace("bookmarks_group", { buffer = bufnr })
-    --             vim.api.nvim_buf_clear_namespace(bufnr, Decorations.ns_id, 0, -1)
-    --         end
-    --     end,
-    -- })
-    --
     -- Clean up on buffer delete
     vim.api.nvim_create_autocmd({ "BufDelete" }, {
         group = group,
--- lua/bookmarks/decorations.lua
+++ lua/bookmarks/decorations.lua
@@ -1,15 +1,14 @@
 local Decorations = {}
-local Utils = require('bookmarks.utils')
+-- local Utils = require('bookmarks.utils')
 
 Decorations.ns_id = vim.api.nvim_create_namespace("bookmarks_hl_ns")
 
 function Decorations.setup(opts)
     vim.api.nvim_set_hl(0, "BookmarkHighlight", {
-        bg = "#49443C",
+        bg = "#594d3e",
         sp = "#FFE5B4",
-        blend = 0, -- No transparency
         bold = true,
-        underline = true,
+        underline = false,
         default = true,
         cterm = {
             bold = true,
@@ -34,13 +33,13 @@ function Decorations.setup(opts)
         hl_mode = "combine", -- or "replace"
     })
     local hl_exists = vim.api.nvim_get_hl(0, { name = "BookmarkHighlight" })
-    Utils.debug_print("BookmarkHighlight definition:", vim.inspect(hl_exists))
+    -- Utils.debug_print("BookmarkHighlight definition:", vim.inspect(hl_exists))
 end
 
 function Decorations.place_signs(bufnr, bookmarks)
-    Utils.debug_print(string.format("Placing signs for buffer %d", bufnr))
+    -- Utils.debug_print(string.format("Placing signs for buffer %d", bufnr))
     if not vim.api.nvim_buf_is_valid(bufnr) then
-        Utils.debug_print(string.format("Buffer %d is not valid", bufnr))
+        -- Utils.debug_print(string.format("Buffer %d is not valid", bufnr))
         return
     end
 
@@ -49,7 +48,7 @@ function Decorations.place_signs(bufnr, bookmarks)
     local bookmark_array = bookmarks.items or bookmarks
 
     if not bookmark_array or #bookmark_array == 0 or current_filename == "" then
-        Utils.debug_print("No bookmarks to place signs for")
+        -- Utils.debug_print("No bookmarks to place signs for")
         vim.fn.sign_unplace("bookmarks_group", { buffer = bufnr })
         return
     end
@@ -58,29 +57,10 @@ function Decorations.place_signs(bufnr, bookmarks)
     vim.fn.sign_unplace("bookmarks_group", { buffer = bufnr })
 
     -- Place signs
-    -- for _, bmk in ipairs(bookmark_array) do
-    --     if bmk.filename == current_filename and
-    --         bmk.line > 0 and
-    --         bmk.line <= vim.api.nvim_buf_line_count(bufnr) then
-    --         vim.fn.sign_place(
-    --             0,
-    --             "bookmarks_group",
-    --             "BookmarkSign",
-    --             bufnr,
-    --             {
-    --                 lnum = bmk.line,
-    --                 priority = 10
-    --             }
-    --         )
-    --     end
-    -- end
     for _, bmk in ipairs(bookmark_array) do
         if bmk.filename == current_filename and
             bmk.line > 0 and
             bmk.line <= vim.api.nvim_buf_line_count(bufnr) then
-
-              Utils.debug_print(string.format("Placing sign at line %d", bmk.line))
-            -- Place sign
             vim.fn.sign_place(
                 0,
                 "bookmarks_group",
@@ -90,17 +70,6 @@ function Decorations.place_signs(bufnr, bookmarks)
                     lnum = bmk.line,
                     priority = 10
                 }
-            )
-
-            -- Add highlight using nvim_buf_add_highlight
-            local line_idx = bmk.line - 1 -- Convert to 0-based index
-            vim.api.nvim_buf_add_highlight(
-                bufnr,
-                Decorations.ns_id,
-                "BookmarkHighlight",
-                line_idx,
-                0,
-                -1
             )
         end
     end
@@ -121,28 +90,10 @@ function Decorations.highlight_lines(bufnr, bookmarks)
     local current_filename = vim.api.nvim_buf_get_name(bufnr)
     local line_count = vim.api.nvim_buf_line_count(bufnr)
 
-    Utils.debug_print("Starting line highlight")
+    -- Utils.debug_print("Starting line highlight")
 
-    -- for _, bmk in ipairs(bookmark_array) do
-    --     Utils.debug_print(string.format("Highlighting line %d in file %s", bmk.line, bmk.filename))
-    --     if bmk.filename == current_filename then
-    --         local zero_based_line = bmk.line - 1
-    --         if zero_based_line >= 0 and zero_based_line < line_count then
-    --             vim.api.nvim_buf_add_highlight(
-    --                 bufnr,
-    --                 Decorations.ns_id,
-    --                 "BookmarkHighlight",
-    --                 zero_based_line,
-    --                 0,
-    --                 -1
-    --             )
-    --             Utils.debug_print(string.format("Added highlight at line %d", bmk.line))
-    --         end
-    --     end
-    -- end
-    --
     for _, bmk in ipairs(bookmark_array) do
-        Utils.debug_print(string.format("Highlighting line %d in file %s", bmk.line, bmk.filename))
+        -- Utils.debug_print(string.format("Highlighting line %d in file %s", bmk.line, bmk.filename))
         if bmk.filename == current_filename then
             local zero_based_line = bmk.line - 1
             if zero_based_line >= 0 and zero_based_line < line_count then
@@ -154,7 +105,7 @@ function Decorations.highlight_lines(bufnr, bookmarks)
                     0,
                     -1
                 )
-                Utils.debug_print(string.format("Added highlight at line %d", bmk.line))
+                -- Utils.debug_print(string.format("Added highlight at line %d", bmk.line))
             end
         end
     end
